de604c602163ff51c61bc60cbbaacd0a3e8670d4
milos381 <milos_popovic@outlook.com>
2021-04-12 21:28:04 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import SimpleComponent from './components/SimpleComponent';
 import SimplerComponent from './components/SimplerComponent';
 
 ReactDOM.render(
+
   <div>
     <SimpleComponent />
     <SimplerComponent handleClick={undefined} />
